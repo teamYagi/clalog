51e92fd6e1667c392ee230d1acdb4eea5276e064
Yuto Taniguchi <youdougukou5@gmail.com>
2024-05-04 21:24:38 +0900

    u!

--- app/page.tsx
+++ app/page.tsx
@@ -1,46 +1,18 @@
-import Image from "next/image";
-import Head from "next/head"; 
-import { createClient } from "@supabase/supabase-js";
-import { useEffect, useState } from "react";
-
-const supabaseUrl = "https://qeyuzvbfjkxzmklnurig.supabase.co";
-const supabaseKey = process.env.SUPABASE_KEY;
-const supabase = createClient(supabaseUrl, supabaseKey || '');
-
-const TABLE_NAME = "uni_classes";
-
-export const fetchUniClasses = async () => {
-  try {
-    const { data } = await supabase.from(TABLE_NAME).select("uni_classes:name");
-    return data;
-  } catch (error) {
-    console.error(error);
-  }
-};
+import Head from "next/head"; // Next.jsのHeadコンポーネントをインポート
+import React from "react";
+import styles from "./styles.module.css";
 
 export default function Home() {
-  const [uniClasses, setUniClasses] = useState([]);
-
-  useEffect(() => {
-    async function fetchData() {
-      const data = await fetchUniClasses();
-      setUniClasses(data);
-    }
-    fetchData();
-  }, []);
-
   return (
     <div>
       <Head>
-        <link rel="stylesheet" type="text/css" href="styles.css" />
+        {" "}
+        {/* Headコンポーネントを使用してページのヘッダー情報を設定 */}
+        {/* CSSファイルのパスはルート相対パスで指定 */}
       </Head>
       <main>
-        <div className="header">こここ</div>
-        <ul>
-          {uniClasses.map((classItem, index) => (
-            <li key={index}>{classItem.name}</li>
-          ))}
-        </ul>
+        <div className={styles.button}>ko</div>{" "}
+        {/* class属性はclassNameに変更 */}
       </main>
     </div>
   );
