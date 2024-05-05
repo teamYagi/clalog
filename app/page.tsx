3e8cf64d67ca59f40c4595105e63514b297bb0fc
humimasusan <82369281+humimasusan@users.noreply.github.com>
2024-05-05 15:35:39 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
+
 // pages/index.tsx
 "use client";
-=======
-import Head from "next/head"; // Next.jsのHeadコンポーネントをインポート
-import React from "react";
-import styles from "./styles.module.css";
->>>>>>> 51e92fd6e1667c392ee230d1acdb4eea5276e064
 
 import Button from "../component/button";
 import InputField from "../component/textbox";
@@ -25,16 +20,6 @@ const Home: React.FC = () => {
   };
   return (
     <div>
-<<<<<<< HEAD
-      <h1>大学授業検索</h1>
-      <InputField
-        value={inputValue}
-        onChange={handleInputChange}
-        placeholder="検索ワードを入力"
-      />
-      <Button onClick={handleSearchClick} text="検索" />
-      {searchResult && <p>{searchResult}</p>}
-=======
       <Head>
         {" "}
         {/* Headコンポーネントを使用してページのヘッダー情報を設定 */}
